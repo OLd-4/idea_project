1c165cc8f6425b6aba9ded5b1076325580f834a1
jayce <jayce@163.com>
2022-10-03 21:39:21 +0800

    First Commit

---
+++ src/main/java/org/jayce/demo/Demo1.java
@@ -0,0 +1,14 @@
+package org.jayce.demo;
+
+/**
+ * @Author: OLd-4
+ * @Description: Git
+ * @Date: 2022/10/3
+ */
+public class Demo1 {
+
+    public static void main(String[] args) {
+        System.out.println("hello!");
+    }
+
+}
